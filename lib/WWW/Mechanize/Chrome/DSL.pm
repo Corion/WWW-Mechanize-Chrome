7501d8cf33ad9d22dcd2cbf5e172984a7ea9b38d
Max Maischein <corion@corion.net>
2017-05-08 20:35:47 +0200

    Add WWW::Mechanize::Chrome::DSL

---
+++ lib/WWW/Mechanize/Chrome/DSL.pm
@@ -0,0 +1,77 @@
+package WWW::Mechanize::Chrome::DSL;
+use strict;
+use WWW::Mechanize::Chrome;
+use Object::Import;
+use Carp qw(croak);
+
+use vars qw($VERSION @CARP_NOT);
+$VERSION = '0.01';
+
+@CARP_NOT = (qw[
+    WWW::Mechanize::Chrome
+]);
+
+=head1 NAME
+
+WWW::Mechanize::Chrome::DSL - Domain Specific Language for short scripts
+
+=head1 SYNOPSIS
+
+    use WWW::Mechanize::Chrome::DSL '$mech';
+
+    get 'http://google.de';
+    
+    my @links = selector('a');
+    print $_->{innerHTML},"\n" for @links;
+    
+    click($links[0]);
+    
+    print content;
+
+This module exports all methods of one WWW::Mechanize::Chrome
+object as subroutines. That way, you can write short scripts without
+cluttering every line with C<< $mech-> >>.
+
+This module is highly experimental and might vanish from the distribution
+again if I find that it is useless.
+
+=cut
+
+sub import {
+    my ($class, %options);
+    if (@_ == 2) {
+        ($class, $options{ name }) = @_;
+    } else {
+        ($class, %options) = @_;
+    };
+    my $target = delete $options{ target } || caller;
+    my $name = delete $options{ name } || '$mech';
+    my $mech = WWW::Mechanize::Chrome->new(%options);
+    
+    $name =~ s/^[\$]//
+        or croak 'Variable name must start with $';
+    {
+        no strict 'refs';
+        *{"$target\::$name"} = \$mech;
+        import Object::Import \${"$target\::$name"},
+                              deref => 1,
+                              target => $target,
+                              ;
+    };
+};
+
+1;
+
+=head1 AUTHORS
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2009-2017 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
---
+++ t/50-mech-new-dsl.t
@@ -0,0 +1,46 @@
+#!perl -w
+use strict;
+use Test::More;
+use File::Basename;
+use WWW::Mechanize::Chrome;
+
+use lib 'inc', '../inc', '.';
+use Test::HTTP::LocalServer;
+
+use t::helper;
+
+# What instances of Chrome will we try?
+my $instance_port = 9222;
+my @instances = t::helper::browser_instances();
+
+if (my $err = t::helper::default_unavailable) {
+    plan skip_all => "Couldn't connect to Chrome: $@";
+    exit
+} else {
+    plan tests => 1*@instances;
+};
+
+sub new_mech {
+    WWW::Mechanize::Chrome->new(
+        autodie => 1,
+        log => sub {},
+        @_,
+    );
+};
+
+use vars '$mech';
+
+t::helper::run_across_instances(\@instances, $instance_port, \&new_mech, 12, sub {
+    my( $file, $mymech ) = splice @_; # so we move references
+
+    use WWW::Mechanize::Chrome::DSL ();
+    Object::Import->import( $mymech );
+    
+    get_local( '49-mech-get-file.html' );
+    sleep 1;
+    is title(), '49-mech-get-file.html', 'We opened the right page';
+    #is ct(), 'text/html', "Content-Type is text/html";
+    diag uri();
+
+    undef $mech;
+});
