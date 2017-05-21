a5a45300436dcd226edd248a7fa4c1ba45f1f328
Max Maischein <corion@corion.net>
2017-05-21 19:33:31 +0200

    Silence shutdown

--- lib/Chrome/DevToolsProtocol.pm
+++ lib/Chrome/DevToolsProtocol.pm
@@ -70,7 +70,6 @@ sub log( $self, $level, $message, @args ) {
 sub connect( $self, %args ) {
     # If we are still connected to a different tab, disconnect from it
     if( $self->transport and ref $self->transport ) {
-        warn "Reusing self, closing websocket";
         $self->transport->close();
     };
 
