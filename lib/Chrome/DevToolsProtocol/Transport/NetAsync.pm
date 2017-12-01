70dc3d554e82a408ecfe0ab2d4aa1a6d575c3d89
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2017-12-01 20:59:44 +0000

    Also log when websocket connection gets closed

--- lib/Chrome/DevToolsProtocol/Transport/NetAsync.pm
+++ lib/Chrome/DevToolsProtocol/Transport/NetAsync.pm
@@ -53,6 +53,11 @@ sub connect( $self, $handler, $got_endpoint, $logger ) {
                 $logger->('trace', "Got message", $message );
                 $handler->on_response( $connection, $message )
             },
+            on_read_eof => sub {
+                my( $connection )=@_;
+                $logger->('info', "Connection closed");
+                # TODO: should we tell handler?
+            },
         );
         $self->loop->add( $client );
 
