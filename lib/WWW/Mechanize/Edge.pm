4836e76e35dbf2a1693216c10a18827813b00db0
Max Maischein <corion@corion.net>
2018-09-15 21:09:07 +0200

    Add WWW::Mechanize::Edge
    Even though we can only connect and launch an URL, it already
    improves the Chrome code by making it more modular
    
    # Conflicts:
    #	edge.pl

---
+++ lib/WWW/Mechanize/Edge.pm
@@ -0,0 +1,74 @@
+package WWW::Mechanize::Edge;
+use Carp qw(croak);
+use Moo;
+use Filter::signatures;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+
+extends 'WWW::Mechanize::Chrome';
+
+# c:\Users\Corion\AppData\Local\Microsoft\WindowsApps\MicrosoftEdge.exe
+# Returns additional directories where the default executable can be found
+# on this OS
+sub additional_executable_search_directories( $class, $os_style=$^O ) {
+    my @search;
+    if( $os_style =~ /MSWin/i ) {
+        push @search,
+            map { "$_\\Microsoft\\WindowsApps" }
+            grep {defined}
+            ($ENV{'LOCALAPPDATA'},
+            );
+    }
+    @search
+}
+
+sub default_executable_names( $class, @other ) {
+    my @program_names
+        = grep { defined($_) } (
+        $ENV{EDGE_BIN},
+        @other,
+    );
+    if( ! @program_names ) {
+        push @program_names,
+          $^O =~ /mswin/i ? 'MicrosoftEdge.exe'
+        : ()
+    };
+    @program_names
+}
+
+sub build_command_line( $class, $options ) {
+    my @program_names = $class->default_executable_names( $options->{launch_exe} );
+    warn "[[@program_names]]";
+    my( $program, $error) = $class->find_executable(\@program_names);
+    croak $error if ! $program;
+
+    $options->{port} ||= 9222
+      if ! exists $options->{port};
+
+    push @{ $options->{ launch_arg }}, '--devtools-server-port', $options->{ port };
+
+   $options->{ launch_arg } ||= [];
+    # We will need a magic cookie so we find the tab that pops up
+    $options->{ start_url } ||= "about:blank";
+    
+    push @{ $options->{ launch_arg }}, "$options->{start_url}"
+        if exists $options->{start_url};
+    my @cmd=( $program, @{ $options->{launch_arg}} );
+
+    @cmd
+};
+
+sub _setup_driver_future( $self, %options ) {
+    $self->driver->connect(
+        #new_tab => !$options{ reuse },
+        tab     => qr/^about:blank$/i,
+    )->catch( sub(@args) {
+        my $err = $args[0];
+        if( ref $args[1] eq 'HASH') {
+            $err .= $args[1]->{Reason};
+        };
+        Future->fail( $err );
+    })
+}
+
+1;
